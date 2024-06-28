08f66066e2d5b297c96b9d105e5b1551fc3adaed
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-06-28 16:46:41 +0545

    added a compound component

---
+++ src/components/constants/post.ts
@@ -0,0 +1,13 @@
+import { TPost } from "../../types/TPost";
+
+const postData: TPost = {
+  id: 1,
+  title: "Compound Components",
+  content: "This is a Compound PostCard component",
+  user: {
+    id: 1,
+    name: "John Doe",
+  },
+};
+
+export default postData;
